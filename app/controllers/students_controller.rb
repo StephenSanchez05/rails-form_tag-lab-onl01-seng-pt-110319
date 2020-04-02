b1008a04888c20ae0a566a065bd94e76f86ca2a8
stephensanchez05 <stephensanchez05@gmail.com>
2020-04-02 10:46:39 -0400

    a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,5 +15,10 @@ class StudentsController < ApplicationController
   session[:form_params] = params.inspect
   redirect_to new_student_path
   end
+  private
 
+    def set_student
+      @student = Student.find(params[:id])
+    end
+end
 end
